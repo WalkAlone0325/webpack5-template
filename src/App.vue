c66319fea7263faf215f8b4f178c5af386741dcb
独行 <jsw0325@163.com>
2021-02-23 18:32:34 +0800

    chore: 构建 webpack5

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>App page</h1>
+    <p>new</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const testFunction = () => {
+      console.log('test')
+    }
+    return { testFunction }
+  },
+}
+</script>
+
+<style>
+div {
+  color: yellowgreen;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
+
+// const root = document.getElementById('app')
+
+// root.textContent = 'hello webpack5 and vue3'
